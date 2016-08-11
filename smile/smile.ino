44c184fa9c60d7cbc495c5c1a501670c302332d2
Rory Hayes <roryohayes@gmail.com>
2016-08-10 17:23:39 -0700

    but why tho

--- smile/smile.ino
+++ smile/smile.ino
@@ -26,8 +26,8 @@ void setup() {
 }
 
 void loop() {
-  for (int i=0; i<=255; i++){
-    color = Wheel(i);
+  for (int i=0; i<360; i++){
+    color = getRGBfromHue(i);
     
     // :
     pixels.setPixelColor(1, color);
@@ -56,7 +56,57 @@ void loop() {
     pixels.setPixelColor(27, color);
     
     pixels.show();
+
+    delay(50);
   }
+}
+
+// takes int 0-360 and translates from HSV to RGB
+// I basically implemented wikipedia: https://en.wikipedia.org/wiki/HSL_and_HSV#/media/File:HSV-RGB-comparison.svg
+uint32_t getRGBfromHue(int hue) {
+  double r, g, b = 0;
+  int i=(int)floor(hue/60.0); // which section we're in
+  double f = hue/60.0 - i; // how far we are through this section
+
+  switch (i) {
+    case 0:
+      r = 1;
+      g = f;
+      b = 0;
+      break;
+    case 1:
+      r = 1-f;
+      g = 1;
+      b = 0;
+      break;
+    case 2: 
+      r = 0;
+      g = 1;
+      b = f;
+      break;
+    case 3:
+      r = 0;
+      g = 1-f;
+      b = 1;
+      break;
+    case 4:
+      r = f;
+      g = 0;
+      b = 1;
+      break;
+    case 5:
+      r = 1;
+      g = 0;
+      b = 1-f;
+      break;
+  }
+
+  //set each component to a integer value between 0 and 255
+  int red = constrain((int)255*r,0,255);
+  int green = constrain((int)255*g,0,255);
+  int blue = constrain((int)255*b,0,255);
+
+  return pixels.Color(red, green, blue);
 }
 
 uint32_t Wheel(byte WheelPos) {
