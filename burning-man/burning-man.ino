349b66208873be614951ae3c52cc6cb61a6dee8d
Rory Hayes <roryohayes@gmail.com>
2016-08-24 00:22:24 -0700

    customize brightness and some new effects

--- burning-man/burning-man.ino
+++ burning-man/burning-man.ino
@@ -2,6 +2,8 @@
 // 16 is left eye top (between star and data out)
 // inc clockwise looking out
 
+// todo: figure out this uint32_t, etc shit.  can i save more space?
+
 #include <Adafruit_NeoPixel.h>
 
 #ifdef __AVR_ATtiny85__ // Trinket, Gemma, etc.
@@ -12,10 +14,12 @@
 #define LED_LENGTH 32
 #define BUTTON1_PIN 3
 #define BUTTON2_PIN 2
+#define MODE_SWITCH_MILLIS 10000
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LED_LENGTH, LED_PIN);
 
 int smileyLEDS[] = {1, 15, 5, 6, 7, 8, 9, 10, 11, 17, 31, 21, 22, 23, 24, 25, 26, 27};
+int horizontalLEDS[] = {4, 4, 3, 5, 2, 6, 1, 7, 0, 8, 15, 9, 14, 10, 13, 11, 12, 12};
 
 bool button1OldState = HIGH;
 bool button2OldState = HIGH;
@@ -24,6 +28,8 @@ bool button2OldState = HIGH;
 int hue = 0;
 // keep track of each mode's progress
 int modeCounter = 0;
+// fall back brightness, default to 1/3
+int brightness = 85;
 
 int mode = 0;
 uint32_t lastModeChange;
@@ -42,44 +48,67 @@ void setup() {
 
 void loop() {
   // Initialize all LEDs to off and reset brightness
-  setAllPixels(0x000000);
-  pixels.setBrightness(85); // 1/3 brightness
+  //setAllPixels(0x000000);
+  pixels.setBrightness(brightness);
 
   // Update current button state
   bool button1NewState = digitalRead(BUTTON1_PIN);
   bool button2NewState = digitalRead(BUTTON2_PIN);
 
   // Button and State machine management
+  if (button1NewState == LOW && button2NewState == LOW) {
+    brightness = hue;
+  }
   if (button1NewState == LOW && button1OldState == HIGH) {
     if (mode >= 0 || mode <= -3) mode = -1;
     else mode--;
     lastModeChange = millis();
   }
   else if (button2NewState == LOW && button2OldState == HIGH) {
-    if (mode < 0 || mode >= 2) mode = 0;
+    if (mode < 0 || mode >= 4) mode = 0;
     else mode++;
     lastModeChange = millis();
   }
-  else if (mode >= 0 && (millis() > lastModeChange+5000)) {
-    if (mode < 0 || mode >= 2) mode = 0;
+  else if (mode >= 0 && (millis() > lastModeChange + MODE_SWITCH_MILLIS)) {
+    if (mode < 0 || mode >= 4) mode = 0;
     else mode++;
     lastModeChange = millis();
   }
 
   // Flashy light stuff time!
   switch (mode) {
-    // -3: flashlight
+    // -3: flashlight; force max brightness
     case -3: pixels.setBrightness(255); setAllPixels(0xFFFFFF); break;
-    // -2: winky face 0xFFD700
+    // -2: winky face, using color 0xFFD700
     case -2: setSmileyPixels(0xFFD700); wink(); break;
-    // -1: smiley face 0xFFD700
+    // -1: smiley face, cycling rainbow for now!
     case -1: setRainbowSmileyPixels(); break;
     // 0: uniform rainbow cycle
     case 0: setAllPixels(getRainbow(hue)); break;
-    // 1: opposite rainbow spinners
-    case 1: oppositeSpin(getRainbow(hue)); break;
-    case 2: oppositeSpin(0xFF00FF); break;
+    // 1: colorwipe the rainbow cycle
+    case 1: colorWipe(getRainbow(hue)); break;
+    // 2: opposite rainbow spinners
+    case 2: oppositeSpin(getRainbow(hue)); break;
+    // 3: rainbow thing
+    case 3:  rainbowCycle(); break;
+    // 4: do some cool sin and cos shit to get waves of colors moving around your eyes
+    // make this mode inaccessible and unaffected by the timer?
+    case 4: {
+      if (modeCounter >= 9) modeCounter = 0;
+      pixels.setPixelColor(horizontalLEDS[modeCounter*2], getRainbow(hue));
+      pixels.setPixelColor(horizontalLEDS[modeCounter*2+1], getRainbow(hue));
+      pixels.show();
+      delay(50);
+      hue += 10;
+      modeCounter++;
+      break;
+    }
   }
+
+  // ok colorWipe is a cool transition actually, use it for 0
+  // possibly make another mode that wipes continuously updating (is that what rainbow() does?)
+  // i love rainbowCycle
+  // 
 
   // Update the LEDs
   pixels.show();
@@ -95,7 +124,18 @@ void loop() {
   delay(10);
 }
 
+void rainbowCycle() {
+  uint16_t i, j;
+    for(i=0; i < pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, getRainbow(((i / pixels.numPixels()) ) & 255));
+    }
+    pixels.show();
+    delay(20);
+  
+}
+
 // each eye rotates two opposite LED patches
+// todo: randomize the number of LED patches
 void oppositeSpin(uint32_t color) {
   if (modeCounter >= 16) modeCounter = 0;
   for (int i = 0; i < 16; i++) {
@@ -136,6 +176,17 @@ void setAllPixels(uint32_t color) {
   for (int i = 0; i < LED_LENGTH; i++) {
     pixels.setPixelColor(i, color);
   }
+}
+
+// todo: try dividing by two?
+void colorWipe(uint32_t color) {
+  if (modeCounter >= pixels.numPixels()/2) modeCounter = 0;
+    pixels.setPixelColor(modeCounter, color);
+    pixels.setPixelColor(modeCounter+16, color);
+    pixels.show();
+    hue += 10; // the delta between this value and 14 is what makes this interesting visually
+    delay(50);
+    modeCounter++;
 }
 
 // Input a value 0 to 255 to get a color value from the color wheel
